5ae4fd4648463ee942ae26eae60914760ada98a6
RafaelDoering <rafael.doering.soares@usp.br>
2019-04-06 19:25:19 -0300

    Aula 4

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb+srv://admin:admin123@ucldb-rvys5.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
- 
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -13,4 +16,4 @@ app.get('/', (req, res) => res.send('UCL turma 2'))
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
  
-app.listen(3000)
+app.listen(5000);
